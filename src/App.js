bac8f4eb1c2096bb1682362832bfd52652f471be
sugampalav <sugam.palav@pixacore.in>
2021-12-15 19:19:41 +0530

    Netlify upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import "./App.css";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import Home from "./components/pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
 import AboutUs from "./components/pages/AboutUs";
+// import Home from "./components/pages/Home";
 
 function App() {
   return (
     <>
-      <AboutUs />
+      <BrowserRouter>
+        <Navbar />
+        {/* <Home /> */}
+        <AboutUs />
 
-      {/* <Routes>
-        <Route path="/" exact component={Home} />
-        <Route path="/AboutUs" exact component={AboutUs} />
-      </Routes> */}
+        <Routes>
+          <Route path="/AboutUs" exact component={AboutUs} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -1,12 +1,47 @@
 import React from "react";
-import Footer from "../Footer";
-import Navbar from "../Navbar";
+import "./AboutUs.css";
 
 function AboutUs() {
   return (
     <>
-      <Navbar />
-      <Footer />
+      <section className="content-container">
+        <div className="content-wrapper">
+          <h1>About PairsAuto</h1>
+          <p>
+            PAIRS Auto is a local, fully featured auto shop in NE Calgary with
+            years of experience and comprehensive ability. We founded PAIRS Auto
+            to offer our customers the best auto repair experience in Calgary.
+            It can be challenging to find a team of car mechanics who prize
+            honesty as their expertise, who strive to deliver the best customer
+            experience at an affordable price. Those are PAIRS Auto mechanics
+            ethics. With a team of reliable mechanics, we are aiming to provide
+            the best auto services at an affordable price to our customers.
+          </p>
+          <p>
+            Our services vary from Oil Change, Tire Change, AC Recharge, Wheel
+            Alignment, Car Detailing, Windshield Replacement, Mechanical
+            Repairs, Diagnosis, Car Services, Remote Starter and Smart key
+            configuration. We will provide you great auto repair with best price
+            guarantee in Alberta.
+          </p>
+          <p>
+            Taking time to understand any potential problems with your vehicle
+            is the most important part of out job. The diagnostics are an
+            essential part of out auto repair service in Calgary. Our team uses
+            the best modern technologies to pinpoint the exact issue that is
+            affecting the performance of your car. After completing the
+            diagnostics, we will offer you our auto service. If you order the
+            auto repair, we will fix your car and get you back out on the road
+            without any need to return.
+          </p>
+          <p>
+            Any car can break down at any time of day and we understand that,
+            which is why we offer a flexible auto repair service nearby. Only
+            with a quick search of a car repair near me, you can get in touch
+            with the PAIRS Auto team.
+          </p>
+        </div>
+      </section>
     </>
   );
 }

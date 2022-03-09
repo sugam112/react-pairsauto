5146d61116050ecd0feb5c8161925b6c4cd1d576
sugampalav <sugam.palav@pixacore.in>
2022-03-09 23:27:28 +0530

    proxy removed, cards aligned

--- src/components/InventoryCard.js
+++ src/components/InventoryCard.js
@@ -208,49 +208,53 @@ const InventoryCard = () => {
           </Card>
         )}
       </div> */}
-
-      <Row xs={1} md={2} className="g-0">
-        {content ? (
-          dummy.map((dummyData) => {
-            return (
-              <Card
-                variant="top"
-                className="col-md-4 col-xl-3 col-12 m-4 "
-                key={dummyData.id}
-              >
-                <Card.Img variant="top" src="./images/card-1.png" />
-                <Card.Body>
-                  <Card.Title>{dummyData.car_make}</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">
-                    {dummyData.car_model} {dummyData.car_year}
-                  </Card.Subtitle>
-                  <Card.Text> {dummyData.emi}/month</Card.Text>
-                  <Card.Text>{dummyData.price}</Card.Text>
-                  <Card.Text> {dummyData.kms} kms</Card.Text>
-                  <Button href="/contact-us" variant="primary">
-                    More details
-                  </Button>
-                </Card.Body>
-              </Card>
-            );
-          })
-        ) : (
-          <Card className="col-sm-6 col-md-4 col-xl-3 col-12">
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Placeholder as={Card.Title} animation="glow">
-                <Placeholder xs={6} />
-              </Placeholder>
-              <Placeholder as={Card.Text} animation="glow">
-                <Placeholder xs={7} /> <Placeholder xs={4} />{" "}
-                <Placeholder xs={4} /> <Placeholder xs={6} />{" "}
-                <Placeholder xs={8} />
-              </Placeholder>
-              <Placeholder.Button variant="primary" xs={6} />
-            </Card.Body>
-          </Card>
-        )}
-      </Row>
+      <div className="inventory-container">
+        <Row>
+          <input type="text"></input>
+        </Row>
+        <Row xs={1} md={2} className="g-0 d-flex justify-content-around">
+          {content ? (
+            dummy.map((dummyData) => {
+              return (
+                <Card
+                  variant="top"
+                  className="col-md-4 col-xl-3 col-12 m-2 card-decorations"
+                  key={dummyData.id}
+                >
+                  <Card.Img variant="top" src="./images/card-1.png" />
+                  <Card.Body>
+                    <Card.Title>{dummyData.car_make}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      {dummyData.car_model} {dummyData.car_year}
+                    </Card.Subtitle>
+                    <Card.Text> {dummyData.emi}/month</Card.Text>
+                    <Card.Text>{dummyData.price}</Card.Text>
+                    <Card.Text> {dummyData.kms} kms</Card.Text>
+                    <Button href="/contact-us" variant="primary">
+                      More details
+                    </Button>
+                  </Card.Body>
+                </Card>
+              );
+            })
+          ) : (
+            <Card className="col-sm-6 col-md-4 col-xl-3 col-12">
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Placeholder as={Card.Title} animation="glow">
+                  <Placeholder xs={6} />
+                </Placeholder>
+                <Placeholder as={Card.Text} animation="glow">
+                  <Placeholder xs={7} /> <Placeholder xs={4} />{" "}
+                  <Placeholder xs={4} /> <Placeholder xs={6} />{" "}
+                  <Placeholder xs={8} />
+                </Placeholder>
+                <Placeholder.Button variant="primary" xs={6} />
+              </Card.Body>
+            </Card>
+          )}
+        </Row>
+      </div>
     </>
   );
 };

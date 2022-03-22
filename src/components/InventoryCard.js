d0c03162804f4fd637de965877e7c46aef722e06
sugampalav <sugam.palav@pixacore.in>
2022-03-22 11:49:14 +0530

    .

--- src/components/InventoryCard.js
+++ src/components/InventoryCard.js
@@ -122,17 +122,41 @@ const InventoryCard = (props) => {
 
   const fetchData = async (url) => {
     try {
-      const response = axios.get(url, {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      axios(
+        url,
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Request-Headers": "*",
+            // credentials: "include",
+          },
         },
+        { method: "GET" }
+      ).then((response) => {
+        console.log(response);
+        console.log(response.data);
+        console.log(response.status);
+        console.log(response.statusText);
+        console.log(response.headers);
+        console.log(response.config);
       });
-      const { results } = response.data;
-      setProducts(results);
-      console.log(products);
-    } catch (e) {
-      console.log("Error");
+    } catch (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
     }
   };
 

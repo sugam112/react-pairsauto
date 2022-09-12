c76c1ce4c0f9b23c9d94b8c51a45c5b660f8133d
sugampalav <sugam.palav@pixacore.in>
2022-09-12 20:50:21 +0530

    alpha update

--- src/components/InventoryCard.js
+++ src/components/InventoryCard.js
@@ -1,112 +1,112 @@
 import React, { useEffect, useState } from "react";
-import { Card, Placeholder, Button, Row } from "react-bootstrap";
+import { Card, Button, Row } from "react-bootstrap";
 import XMLParser from "react-xml-parser";
 import Loader from "./Loader";
 import "./InventoryCard.css";
 
 const InventoryCard = (props) => {
-  const dummy = [
-    {
-      id: 1,
-      imgURL: "http://dummyimage.com/167x100.png/5fa2dd/ffffff",
-      car_make: "Toyota",
-      car_model: "Venza",
-      car_year: 2012,
-      price: "$47171.99",
-      emi: "$519.28",
-      kms: 56630,
-    },
-    {
-      id: 2,
-      imgURL: "http://dummyimage.com/108x100.png/ff4444/ffffff",
-      car_make: "Mazda",
-      car_model: "MX-5",
-      car_year: 1995,
-      price: "$44513.68",
-      emi: "$1215.71",
-      kms: 66711,
-    },
-    {
-      id: 3,
-      imgURL: "http://dummyimage.com/133x100.png/dddddd/000000",
-      car_make: "Volvo",
-      car_model: "XC70",
-      car_year: 2007,
-      price: "$26483.34",
-      emi: "$1467.06",
-      kms: 30288,
-    },
-    {
-      id: 4,
-      imgURL: "http://dummyimage.com/188x100.png/ff4444/ffffff",
-      car_make: "Volkswagen",
-      car_model: "Golf",
-      car_year: 1984,
-      price: "$44773.12",
-      emi: "$1880.04",
-      kms: 11889,
-    },
-    {
-      id: 5,
-      imgURL: "http://dummyimage.com/178x100.png/5fa2dd/ffffff",
-      car_make: "Subaru",
-      car_model: "XT",
-      car_year: 1985,
-      price: "$50060.01",
-      emi: "$1138.12",
-      kms: 13635,
-    },
-    {
-      id: 6,
-      imgURL: "http://dummyimage.com/178x100.png/cc0000/ffffff",
-      car_make: "Ford",
-      car_model: "E-Series",
-      car_year: 1985,
-      price: "$24409.89",
-      emi: "$343.88",
-      kms: 42975,
-    },
-    {
-      id: 7,
-      imgURL: "http://dummyimage.com/243x100.png/cc0000/ffffff",
-      car_make: "Nissan",
-      car_model: "Maxima",
-      car_year: 2009,
-      price: "$18482.09",
-      emi: "$1019.90",
-      kms: 73311,
-    },
-    {
-      id: 8,
-      imgURL: "http://dummyimage.com/156x100.png/ff4444/ffffff",
-      car_make: "Mazda",
-      car_model: "RX-8",
-      car_year: 2005,
-      price: "$63417.10",
-      emi: "$680.66",
-      kms: 33821,
-    },
-    {
-      id: 9,
-      imgURL: "http://dummyimage.com/196x100.png/ff4444/ffffff",
-      car_make: "Alfa Romeo",
-      car_model: "164",
-      car_year: 1993,
-      price: "$33988.98",
-      emi: "$767.03",
-      kms: 76108,
-    },
-    {
-      id: 10,
-      imgURL: "http://dummyimage.com/156x100.png/dddddd/000000",
-      car_make: "Saturn",
-      car_model: "VUE",
-      car_year: 2002,
-      price: "$10912.03",
-      emi: "$1771.64",
-      kms: 44607,
-    },
-  ];
+  // const dummy = [
+  //   {
+  //     id: 1,
+  //     imgURL: "http://dummyimage.com/167x100.png/5fa2dd/ffffff",
+  //     car_make: "Toyota",
+  //     car_model: "Venza",
+  //     car_year: 2012,
+  //     price: "$47171.99",
+  //     emi: "$519.28",
+  //     kms: 56630,
+  //   },
+  //   {
+  //     id: 2,
+  //     imgURL: "http://dummyimage.com/108x100.png/ff4444/ffffff",
+  //     car_make: "Mazda",
+  //     car_model: "MX-5",
+  //     car_year: 1995,
+  //     price: "$44513.68",
+  //     emi: "$1215.71",
+  //     kms: 66711,
+  //   },
+  //   {
+  //     id: 3,
+  //     imgURL: "http://dummyimage.com/133x100.png/dddddd/000000",
+  //     car_make: "Volvo",
+  //     car_model: "XC70",
+  //     car_year: 2007,
+  //     price: "$26483.34",
+  //     emi: "$1467.06",
+  //     kms: 30288,
+  //   },
+  //   {
+  //     id: 4,
+  //     imgURL: "http://dummyimage.com/188x100.png/ff4444/ffffff",
+  //     car_make: "Volkswagen",
+  //     car_model: "Golf",
+  //     car_year: 1984,
+  //     price: "$44773.12",
+  //     emi: "$1880.04",
+  //     kms: 11889,
+  //   },
+  //   {
+  //     id: 5,
+  //     imgURL: "http://dummyimage.com/178x100.png/5fa2dd/ffffff",
+  //     car_make: "Subaru",
+  //     car_model: "XT",
+  //     car_year: 1985,
+  //     price: "$50060.01",
+  //     emi: "$1138.12",
+  //     kms: 13635,
+  //   },
+  //   {
+  //     id: 6,
+  //     imgURL: "http://dummyimage.com/178x100.png/cc0000/ffffff",
+  //     car_make: "Ford",
+  //     car_model: "E-Series",
+  //     car_year: 1985,
+  //     price: "$24409.89",
+  //     emi: "$343.88",
+  //     kms: 42975,
+  //   },
+  //   {
+  //     id: 7,
+  //     imgURL: "http://dummyimage.com/243x100.png/cc0000/ffffff",
+  //     car_make: "Nissan",
+  //     car_model: "Maxima",
+  //     car_year: 2009,
+  //     price: "$18482.09",
+  //     emi: "$1019.90",
+  //     kms: 73311,
+  //   },
+  //   {
+  //     id: 8,
+  //     imgURL: "http://dummyimage.com/156x100.png/ff4444/ffffff",
+  //     car_make: "Mazda",
+  //     car_model: "RX-8",
+  //     car_year: 2005,
+  //     price: "$63417.10",
+  //     emi: "$680.66",
+  //     kms: 33821,
+  //   },
+  //   {
+  //     id: 9,
+  //     imgURL: "http://dummyimage.com/196x100.png/ff4444/ffffff",
+  //     car_make: "Alfa Romeo",
+  //     car_model: "164",
+  //     car_year: 1993,
+  //     price: "$33988.98",
+  //     emi: "$767.03",
+  //     kms: 76108,
+  //   },
+  //   {
+  //     id: 10,
+  //     imgURL: "http://dummyimage.com/156x100.png/dddddd/000000",
+  //     car_make: "Saturn",
+  //     car_model: "VUE",
+  //     car_year: 2002,
+  //     price: "$10912.03",
+  //     emi: "$1771.64",
+  //     kms: 44607,
+  //   },
+  // ];
   const [vehicle, setVehicles] = useState();
 
   const axios = require("axios");
@@ -189,7 +189,7 @@ const InventoryCard = (props) => {
                     <Card.Text> {car.children[10].value}/month</Card.Text>
                     <Card.Text>{car.children[10].value}</Card.Text>
                     <Card.Text> {car.children[10].value} kms</Card.Text>
-                    <Button href="/contact-us" variant="primary">
+                    <Button href="/CarDetails" variant="primary">
                       More details
                     </Button>
                   </Card.Body>
